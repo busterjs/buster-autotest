d24ae95ea7cbc6237a69b8c59435bbe1bf61232d
Christian Johansen <christian@cjohansen.no>
2013-04-14 02:48:37 +0200

    Even simpler test runner

--- run-tests.js
+++ run-tests.js
@@ -1,17 +1,2 @@
 var buster = require("buster-node");
-
-buster.testRunner.onCreate(function (runner) {
-    buster.referee.on("pass", runner.assertionPass.bind(runner));
-
-    runner.on("suite:end", function (results) {
-        if (!results.ok) {
-            setTimeout(function () {
-                process.exit(1);
-            }, 50);
-        }
-    });
-});
-
-buster.testContext.on("create", buster.autoRun());
-
 require("./test/buster-autotest-test");
